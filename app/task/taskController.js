40f8645ff3f88b2e5c6defc9166efb385ddc3d9c
mkingst <44472403+mkingst@users.noreply.github.com>
2018-10-25 20:56:15 +0100

    Added "Create Task" function using Postman

---
+++ app/task/taskController.js
@@ -0,0 +1,40 @@
+//The following takes a json task sent from Postman and creates it in the database
+
+//require database connection
+var connection = require('../lib/database.js').connection;
+
+module.exports = {
+
+    createTask: function (req, res) {
+
+        //access the data in the reqest body
+        var requestData = req.body;
+
+        //if task message is not available, then return an error (test this by misspelling te request in Postman)
+        if (!requestData.taskMessage) {
+            return res.status(400).json({ code: "taskCreationgFailed", message: "task message not available" });
+        }
+
+        //populate a mysql table if a task is received
+        var taskData = {
+            taskMessage: requestData.taskMessage,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        };
+
+        connection.query('INSERT INTO task SET ?', taskData, function (err, result, fields) {
+
+            //if an error occurs inserting to the database, show an error
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ code: "taskCreationFailed", message: "Error occured while creating record" });
+            }
+            
+            //otherwise, send a message showing that is was successful
+            return res.status(200).json({ code: "taskCreated", message: "Task creation successfull" });
+
+        });
+
+    }
+
+};
--- application.js
+++ application.js
@@ -1,6 +1,11 @@
 //Require the express module for a http server. Node.js will load this module from the node_modules folder to create a http server
-
 var express = require('express');
+
+//require the body parser module for json and encoded URL's. This will parse the data from the request
+var bodyParser = require('body-parser');
+
+//require the task controller which has the function of creating a task
+var taskController = require('./app/task/taskController');
 
 //include a database connection (database.js)
 var connection = require('./app/lib/database.js').connection;
@@ -10,6 +15,12 @@ var SERVER_PORT = 5000;
 //create express http app instance 
 
 var app = express();
+
+//apply middlewares
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
+
+app.post('/api/v1.0/task',taskController.createTask);
 
 //instanciate my express server on port 5000
 
