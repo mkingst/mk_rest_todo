5a26ce85c04a4348e4d2221d4d59640e0723e7bf
mkingst <44472403+mkingst@users.noreply.github.com>
2018-10-25 15:52:57 +0100

    Initialise commit

---
+++ application.js
@@ -0,0 +1,15 @@
+//Require the express module for a http server. Node.js will load this module from the node_modules folder to create a http server
+
+var express = require('express');
+
+var SERVER_PORT = 5000;
+
+//create express http app instance 
+
+var app = express();
+
+//instanciate my express server on port 5000
+
+app.listen(5000,function(){
+    console.log("Server successfully started on port:" + SERVER_PORT);
+});
